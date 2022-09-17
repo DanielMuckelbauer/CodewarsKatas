e0495cea00b9dc8b8c85de64ad9482b2a4790a9c
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-09-17 19:17:39 +0200

    Parse expression

---
+++ UnknownDigit/Expression.cs
@@ -0,0 +1,9 @@
+﻿namespace UnknownDigit;
+
+public class Expression
+{
+    public string Operand1 { get; set; }
+    public string Operator { get; set; }
+    public string Operand2 { get; set; }
+    public string Result { get; set; }
+}
--- UnknownDigit/UnknownDigitFinder.cs
+++ UnknownDigit/UnknownDigitFinder.cs
@@ -1,9 +1,30 @@
-﻿namespace UnknownDigit;
+﻿using System.Text.RegularExpressions;
+
+namespace UnknownDigit;
 
 public static class UnknownDigitFinder
 {
     public static int SolveExpression(string expression)
     {
+        var parsedExpression = ParseExpression(expression);
+        return SolveExpression(parsedExpression);
+    }
 
+    private static int SolveExpression(Expression expression)
+    {
+        return -1;
+    }
+
+    private static Expression ParseExpression(string expression)
+    {
+        var parts = expression.Split('*', '+', '-', '=');
+        var op = Regex.Match(expression, @"[\+|-|\*|]").Value;
+        return new()
+        {
+            Operand1 = parts[0],
+            Operator = op,
+            Operand2 = parts[1],
+            Result = parts[2]
+        };
     }
 }
