9c220cdfd5dc3182ec8d58dd71c5dbd9086f3361
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2022-09-26 08:01:26 +0200

    More tests

--- UnknownDigit.Tests/UnknownDigitsFinderTests.cs
+++ UnknownDigit.Tests/UnknownDigitsFinderTests.cs
@@ -8,6 +8,12 @@ public class UnknownDigitsFinderTests
     [InlineData("123*45?=5?088", 6)]
     [InlineData("19--45=5?", -1)]
     [InlineData("?*11=??", 2)]
+    [InlineData("-?*11=?", 0)]
+    [InlineData("-?*?=?", 0)]
+    [InlineData("?*51=?", 0)]
     public void Finds_Unknown_Digits(string input, int solution)
         => UnknownDigitFinder.SolveExpression(input).Should().Be(solution);
 }
+
+// ? is always the same digit
+// ? is never an already existing digit
